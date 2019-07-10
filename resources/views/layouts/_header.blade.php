ca7ee2273cd9a72247583b2656af34797f748685
wangxuxu7912 <1150295049@qq.com>
2019-07-10 06:30:23 +0000

    切割头部和尾部子视图

---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: asus
+ * Date: 2019/7/10
+ * Time: 14:26
+ */
+?>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container ">
+        <a class="navbar-brand" href="/">Weibo App</a>
+        <ul class="navbar-nav justify-content-end">
+            <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+            <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
+        </ul>
+    </div>
+</nav>
