b9cfb3c31008a594495687f23765530b5eca2284
wangxuxu7912 <1150295049@qq.com>
2019-07-11 07:18:36 +0000

    社交统计信息

--- app/Models/User.php
+++ app/Models/User.php
@@ -78,7 +78,7 @@ class User extends Authenticatable
         }
         $this->followings()->detach($user_ids);
     }
-    
+
     public function isFollowing($user_id)
     {
         return $this->followings->contains($user_id);
