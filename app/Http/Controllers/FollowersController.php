b38cad49fd95aaa5e3e35b78c00c4a27b00e4ccb
wangxuxu7912 <1150295049@qq.com>
2019-07-11 07:40:43 +0000

    关注按钮

---
+++ app/Http/Controllers/FollowersController.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: asus
+ * Date: 2019/7/11
+ * Time: 15:37
+ */
+
+namespace App\Http\Controllers;
+
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class FollowersController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function store(User $user)
+    {
+        $this->authorize('follow', $user);
+
+        if ( ! Auth::user()->isFollowing($user->id)) {
+            Auth::user()->follow($user->id);
+        }
+
+        return redirect()->route('users.show', $user->id);
+    }
+
+    public function destroy(User $user)
+    {
+        $this->authorize('follow', $user);
+
+        if (Auth::user()->isFollowing($user->id)) {
+            Auth::user()->unfollow($user->id);
+        }
+
+        return redirect()->route('users.show', $user->id);
+    }
+}
